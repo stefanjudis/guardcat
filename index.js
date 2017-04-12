e06c657accb95a0a673a5c44e7fa124dbfd76803
stefan judis <stefanjudis@gmail.com>
2017-04-12 23:53:49 +0200

    add xo code style

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const GitHub = require( 'github-api' );
-const minimatch = require( 'minimatch' );
+const GitHub = require('github-api');
+const minimatch = require('minimatch');
 
 module.exports = {
-  run : ( { token, repoPatterns } = {} ) => {
-    if ( ! token || ! repoPatterns ) {
-      return Promise.reject( new Error( 'Missing required parameter...' ) );
+  run: ({token, repoPatterns} = {}) => {
+    if (!token || !repoPatterns) {
+      return Promise.reject(new Error('Missing required parameter...'));
     }
 
-    const gh = new GitHub( { token } );
+    const gh = new GitHub({token});
     const me = gh.getUser();
 
-    return iterateOverNotifications( {
-      fetch  : me.listNotifications.bind( me ),
-      filter : ( notification ) => {
-        return repoPatterns.reduce( ( result, pattern ) => {
-          return result || minimatch( notification.repository.full_name, pattern );
-        }, false );
+    return iterateOverNotifications({
+      fetch: me.listNotifications.bind(me),
+      filter: notification => {
+        return repoPatterns.reduce((result, pattern) => {
+          return result || minimatch(notification.repository.full_name, pattern);
+        }, false);
       }
-    } );
+    });
   }
 };
 
-function iterateOverNotifications( fns, page = 1, items = [] ) {
-  return fns.fetch( { page } ).then( ( { headers, data } ) => {
-    const regexResult = headers.link.match( /<.*?page=(\d+?)>;\srel="next"/ );
+function iterateOverNotifications(fns, page = 1, items = []) {
+  return fns.fetch({page}).then(({headers, data}) => {
+    const regexResult = headers.link.match(/<.*?page=(\d+?)>;\srel="next"/);
 
-    items = [ ...items, ...data.filter( fns.filter ) ];
+    items = [...items, ...data.filter(fns.filter)];
 
-    if ( regexResult ) {
-      return iterateOverNotifications( fns, regexResult[ 1 ], items )
+    if (regexResult) {
+      return iterateOverNotifications(fns, regexResult[1], items);
     }
 
     return items;
-  } );
+  });
 }
--- test.js
+++ test.js
@@ -1,39 +1,41 @@
 import test from 'ava';
 import proxyquire from 'proxyquire';
 
-const guardCat = proxyquire( './', { 'github-api' : function () {
+// eslint-disable-next-line object-shorthand
+const guardCat = proxyquire('./', {'github-api': function () {
   this.getUser = () => {
     return {
-      listNotifications : ( { page } ) => {
-        return new Promise( resolve => {
-          resolve( require( `./fixtures/page-${ page }` ) );
-        } );
+      listNotifications: ({page}) => {
+        return new Promise(resolve => {
+          resolve(require(`./fixtures/page-${page}`));
+        });
       }
-    }
-  }
-} } );
+    };
+  };
+}});
 
-test( 'should reject with missing params', async t => {
+test('should reject with missing params', async t => {
   try {
     await guardCat.run();
-    await guardCat.run( { token : 'abc' } );
-    await guardCat.run( { repoPatterns : [] } );
-  } catch( e ) {
+    await guardCat.run({token: 'abc'});
+    await guardCat.run({repoPatterns: []});
+  } catch (err) {
     t.pass();
   }
-} );
+});
 
-test( 'should get filtered notifications', async t => {
+test('should get filtered notifications', async t => {
   try {
     const notifications = await guardCat.run(
-      { token : 'abc', repoPatterns : [ 'jessica/*', 'winston/bishop' ] }
+      {token: 'abc', repoPatterns: ['jessica/*', 'winston/bishop']}
     );
 
-    t.is( notifications[ 0 ].id, 1 )
-    t.is( notifications[ 1 ].id, 3 )
-    t.is( notifications[ 2 ].id, 6 )
-    t.is( notifications[ 3 ].id, 7 )
-  } catch( e ) {
+    t.is(notifications[0].id, 1);
+    t.is(notifications[1].id, 3);
+    t.is(notifications[2].id, 6);
+    t.is(notifications[3].id, 7);
+  } catch (err) {
+    console.log(err);
     t.fail();
   }
-} );
+});
